7db36a9133571863b01d150e9b45b5ae702d90ad
pomfy113 <pomfy113@gmail.com>
2018-10-25 15:52:12 -0700

    Not entirely sure if this works to be hoenst

---
+++ graphs-adjacency.py
@@ -0,0 +1,32 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.adjacent = {}
+
+class Graph:
+    def __init__(self, size):
+        self.nodes = [Node(i) for i in range(size)]
+
+    def add_node(self, from_node, to_node, weight):
+        self.nodes[from_node].adjacent[to_node] = weight
+
+    def print_graph(self):
+        for node in self.nodes:
+            print("Current node:", node.data)
+            print("Adjacents:", node.adjacent)
+
+            print("\n")
+
+
+test = Graph(7)
+test.add_node(1, 3, 1)
+test.add_node(1, 4, 1)
+test.add_node(4, 7, 1)
+test.add_node(3, 7, 2)
+test.add_node(0, 3, 2)
+test.add_node(3, 7, 1)
+test.add_node(0, 7, 4)
+
+
+test.print_graph()
