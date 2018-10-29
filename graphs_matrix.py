1ddb88f668bc77055abeaea6b8c24c432cce5704
pomfy113 <pomfy113@gmail.com>
2018-10-29 14:23:08 -0700

    Adjacency matrixes are WEIRD

--- graphs_matrix.py
+++ graphs_matrix.py
@@ -14,46 +14,70 @@ class Vertex:
 class Graph:
     def __init__(self):
         # References to nodes, id - reference
-        # self.vertices = {}
         self.vertices = []
+        self.vertices_ids = {}
 
         self.size = len(self.vertices)
 
-    def add_vertex(self, loc, value):
+    def add_vertex(self, loc, id):
         self.size += 1
         for row in self.vertices:
             row.append(0)
         self.vertices.append([0 for i in range(self.size)])
         self.vertices[loc][loc] = 1
 
+        self.vertices_ids[id] = loc
+
 
     def add_edge(self, source, dest):
         self.vertices[source][dest] = 1;
         self.vertices[dest][source] = 1
 
-    def remove_vertex(self, vertex):
-        pass
+    def remove_vertex(self, id):
+        target_index = self.vertices_ids[id]
+        last_index = self.size - 1
+
+        print("Removing {}".format(id))
+
+        self.vertices[target_index], self.vertices[last_index] = self.vertices[last_index], self.vertices[target_index]
+        print("= = = Testing = = =")
+        self.print_graph()
+        print("= = = Testing Complete = = =")
+
+        self.vertices.pop()
+
+        for row in self.vertices:
+            row[target_index] = row[last_index]
+            row.pop()
+
 
     def remove_edge(self, source, dest):
         self.vertices[source].adjacent.pop(dest)
 
     def print_graph(self):
         print("\nPrinting graph - Matrix")
-        for index, row in enumerate(self.vertices):
-            print(index, row)
+        for row in self.vertices:
+            print(row)
 
 
 
 if __name__ == "__main__":
     g = Graph()
     for i in range(6):
-        g.add_vertex(i, "Value here {}".format(i))
+        g.add_vertex(i, "ID-{}".format(i))
     g.print_graph()
     g.add_edge(0, 1)
     g.print_graph()
 
     g.add_edge(0, 2)
+    g.add_edge(0, 5)
+
     g.print_graph()
+
+    print(g.vertices_ids)
+    g.remove_vertex("ID-3")
+    g.print_graph()
+
 
     # g.add_edge(0, 3)
     # g.add_edge(0, 4)
